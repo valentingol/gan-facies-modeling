4b5fa0cc33bced22822468274f179f6f2a9c9113
Valentin Goldité <valentin.goldite@gmail.com>
2022-07-22 22:11:38 +0200

    ➕ Add pydocstyle manager function

---
+++ utils/github_actions/pydocstyle_manager.py
@@ -0,0 +1,27 @@
+"""Manage Pydocstyle output on workflow."""
+
+import sys
+
+
+def check_output() -> None:
+    """Check output of Pydocstyle.
+
+    Raises
+    ------
+    ValueError
+        If Pydocstyle find errors.
+    """
+    args = sys.argv[1:]
+    for arg in args:
+        if arg.startswith('--n_errors='):
+            n_errors = int(arg.split('=')[1])
+
+    if n_errors > 0:
+        raise ValueError(
+                f'Pydocstyle found {n_errors} errors in python '
+                'docstrings. Please fix them.'
+                )
+
+
+if __name__ == '__main__':
+    check_output()
