268fa467d5643a8d6825304f765e497252829106
Valentin Goldité <valentin.goldite@gmail.com>
2022-07-27 17:54:36 +0200

    ✅ 🚀 Speed up test by testing the loading inside the training and upgrade with EMA

--- tests/utils/sagan/test_trainer.py
+++ tests/utils/sagan/test_trainer.py
@@ -1,10 +1,10 @@
 """Tests for utils/sagan/trainer.py."""
 
-import os
 import os.path as osp
 import shutil
 from typing import Tuple
 
+import numpy as np
 import pytest
 import torch
 from torch.utils.data import DataLoader
@@ -66,24 +66,21 @@ def test_train(data_loaders: Tuple[DataLoader, DataLoader]) -> None:
         assert osp.exists('res/tmp_test/models/discriminator_step_2.pth')
         assert osp.exists('res/tmp_test/samples/images_step_2.png')
         assert osp.exists('res/tmp_test/attention/gen_attn_step_2/attn_0.npy')
+        # Test load_pretrained_model method
+        trainer.load_pretrained_model(2)
         # Remove tmp folders
         shutil.rmtree('res/tmp_test')
 
-    if osp.exists('configs/runs/tmp_test'):
-        shutil.rmtree('configs/runs/tmp_test')
+    # Test get_log_for_wandb method
+    logs = trainer.get_log_for_wandb({'g_loss': torch.tensor(0.3),
+                                      'd_loss': torch.tensor(-0.2),
+                                      'test': torch.tensor(0.2)},
+                                     avg_gammas=[0.5, 0.6, 0.7])
+    expected_logs = {'g_loss': 0.3, 'd_loss': -0.2, 'test': 0.2,
+                     'sum_losses': 0.1, 'avg_gamma1': 0.5, 'avg_gamma2': 0.6,
+                     'avg_gamma3': 0.7}
+    for key, val in expected_logs.items():
+        assert np.isclose(logs[key], val), f'error for key {key}'
 
-
-def test_load_pretrained_model(
-        data_loaders: Tuple[DataLoader, DataLoader]) -> None:
-    """Test load_pretrained_model method."""
-    trainers = build_trainers(data_loaders)
-    trainer = trainers[1]
-    trainer.model_save_path = 'tests/unittest_models'
-    os.makedirs('res/tmp_test/models', exist_ok=True)
-
-    trainer.load_pretrained_model(2)
-
-    # Remove tmp folders
-    shutil.rmtree('res/tmp_test')
     if osp.exists('configs/runs/tmp_test'):
         shutil.rmtree('configs/runs/tmp_test')
