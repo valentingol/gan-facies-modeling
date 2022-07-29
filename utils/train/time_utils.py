b17baad6eb0f485f6a61499b466399fc1cc6b167
Valentin Goldité <valentin.goldite@gmail.com>
2022-07-29 15:52:31 +0200

    :bug: Fix a bug in the eta time when recovered a pre-train model

--- utils/train/time_utils.py
+++ utils/train/time_utils.py
@@ -11,7 +11,7 @@ def get_delta_eta(start_time: float, start_step: int, step: int,
     delta_str = (f'{delta_t // 3600:02d}h'
                  f'{(delta_t // 60) % 60:02d}m'
                  f'{delta_t % 60:02d}s')
-    eta_t = ((total_step+start_step-step-1) * delta_t // (step+1-start_step))
+    eta_t = ((total_step-step-1) * delta_t // (step+1-start_step))
     eta_str = (f'{eta_t // 3600:02d}h'
                f'{(eta_t // 60) % 60:02d}m'
                f'{eta_t % 60:02d}s')
