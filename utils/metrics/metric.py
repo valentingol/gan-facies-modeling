9f714c6a47a143974e34daa7748642991968df84
Valentin Goldité <valentin.goldite@gmail.com>
2022-08-08 18:52:46 +0200

    ✨ Add metrics from Rongier

---
+++ utils/metrics/metric.py
@@ -0,0 +1,135 @@
+"""Wasserstein metric functions."""
+
+from typing import Dict, List, Optional, Tuple, Union
+
+import numpy as np
+from matplotlib import pyplot as plt
+from scipy.stats import wasserstein_distance as w_dist
+
+from utils.metrics.indicators import compute_indicators
+from utils.metrics.visualize import plot_boxes
+
+IndicatorsList = List[Dict[str, List[float]]]
+
+
+def wasserstein_distances(data1: Union[np.ndarray, IndicatorsList],
+                          data2: Union[np.ndarray, IndicatorsList],
+                          connectivity: Optional[int] = None,
+                          unit_component_size: int = 1,
+                          normalization: str = 'none',
+                          save_boxes_path: Optional[str] = None
+                          ) -> Tuple[Dict[str, float],
+                                     Tuple[IndicatorsList, IndicatorsList]]:
+    """Compute Wasserstein metrics of data1 w.r.t reference data2.
+
+    Parameters
+    ----------
+    data1 : np.ndarray, dtype=np.uint8 or List[Dict[str, List[float]]]
+        Batch of images of dims 3 (2D images) or 4 (3D images).
+        Shape (n_images, [depth,] height, with).
+        OR
+        Output of utils.metrics.compute_indicators if already computed.
+    data2 : np.ndarray, dtype=np.uint8 or List[Dict[str, List[float]]]
+        Batch of images of dims 3 (2D images) or 4 (3D images).
+        Shape (n_images, [depth,] height, with).
+        OR
+        Output of utils.metrics.compute_indicators if already computed.
+    connectivity : int
+        Either 1 for 4-neighborhood (6-neighborhood in 3D)
+        or 2 for 8-neighborhood (18-neighborhood in 3D)
+        or 3 for 26-neighborhood in 3D (connectivity must be < 3 in 2D).
+        By default it is set to 2 for 2D images, 3 for 3D images.
+    unit_component_size : int, optional
+        Maximum size to consider a component as a unit component.
+        By default, 1.
+    normalization : str, optional
+        Normalization method. Must be one of 'none', 'normal' or 'linear'.
+        'none': no normalization, 'normal': standardization, 'linear':
+        linear normalization between 0 and 1. By default, 'none'.
+    save_boxes_path : str or None, optional
+        If not None or empty, save the boxes of the metrics to this path.
+        By default, None.
+
+    Raises
+    ------
+    TypeError
+        If data1 or data2 are not of type np.ndarray or list.
+
+    Returns
+    -------
+    metrics : Dict[str, float]
+        List of Wasserstein metrics from data1 w.r.t data2. The keys
+        are indicators name (with corresponding class) and the value is
+        the Wasserstein distance between values of the two classes.
+        Lower values means better similarity.
+    indicators_list_1 : List[Dict[str, List[float]]]
+        List of indicators from data1.
+    indicators_list_2 : List[Dict[str, List[float]]]
+        List of indicators from data2.
+    """
+    # indictors: list of size n_classes (- 1) of dicts :
+    # {indicator_name (str): indicator_values (1D np.ndarray)}
+    if isinstance(data1, np.ndarray):
+        indicators_list_1: list = compute_indicators(
+            data1, connectivity=connectivity,
+            unit_component_size=unit_component_size,
+            normalization=normalization)
+    elif isinstance(data1, list):
+        indicators_list_1 = data1
+    else:
+        raise TypeError('data1 must be either a np.ndarray (data) or a list '
+                        '(output of "compute_indicators").')
+
+    if isinstance(data2, np.ndarray):
+        indicators_list_2: list = compute_indicators(
+            data2, connectivity=connectivity,
+            unit_component_size=unit_component_size,
+            normalization=normalization)
+    elif isinstance(data2, list):
+        indicators_list_2 = data2
+    else:
+        raise TypeError('data2 must be either a np.ndarray (data) or a list '
+                        '(output of "compute_indicators").')
+
+    if len(indicators_list_1) != len(indicators_list_2):
+        print('Warning: data1 and data2 have different number of classes. '
+              f'Found {len(indicators_list_1)} and {len(indicators_list_2)}. '
+              'This behavior is only expected for bad models '
+              '(e.g. during unit tests). The indicators of missing classes '
+              'will be copy from the last one.')
+        len_1, len_2 = len(indicators_list_1), len(indicators_list_2)
+        if len_1 > len_2:
+            indicators_list_2 = (indicators_list_2
+                                 + [indicators_list_2[-1]] * (len_1-len_2))
+        else:
+            indicators_list_1 = (indicators_list_1
+                                 + [indicators_list_1[-1]] * (len_2-len_1))
+    metrics = {}
+    for i, (indicators_1, indicators_2) in enumerate(zip(indicators_list_1,
+                                                         indicators_list_2)):
+        for ind_name in indicators_1:
+            metric_name = ind_name + f'_cls_{i + 1}'
+            metrics[metric_name] = w_dist(indicators_1[ind_name],
+                                          indicators_2[ind_name])
+
+    if save_boxes_path:
+        n_classes = len(indicators_list_1)
+        indicators_list: List[Dict[str, List[float]]] = [{}] * (2 * n_classes)
+        indicators_list[::2] = indicators_list_1
+        indicators_list[1::2] = indicators_list_2
+        indicator_names = [f'class_{i // 2 + 1}' for i in range(2*n_classes)]
+        indicator_colors = ['lightblue', 'lightgreen'] * n_classes
+        plt.figure(figsize=(30, 30))
+        plot_boxes(indicators_list, indicator_names=indicator_names,
+                   indicator_colors=indicator_colors)
+        plt.savefig(save_boxes_path)
+    return metrics, (indicators_list_1, indicators_list_2)
+
+
+if __name__ == '__main__':
+    data = np.load('datasets/gansim_small.npy')
+    data1 = data[:len(data) // 2]
+    data2 = data[len(data) // 2:]
+    print(wasserstein_distances(data1, data2, normalization='none',
+                                unit_component_size=4,
+                                save_boxes_path='perfect_metrics.png')[0])
---
+++ utils/metrics/visualize.py
@@ -0,0 +1,82 @@
+"""Visualization utilities for metrics."""
+
+from collections import OrderedDict
+from typing import Dict, List, Optional
+
+from matplotlib import pyplot as plt
+
+
+def plot_boxes(indicators_list: List[Dict[str, List[float]]],
+               indicator_names: Optional[List[str]] = None,
+               indicator_colors: Optional[List[str]] = None) -> None:
+    """Plot boxes from multiple indicators dicts.
+
+    Parameters
+    ----------
+    indicators_list : List[Dict[str, List[float]]]
+        List of indicators dicts. Each values should be 1D arrays
+        representing an indictor value for each image. The keys are
+        not necessarily the same over the different dicts.
+    indicator_names : list of strs or None, optional
+        List of names for the indicators. If None, the names will
+        be the number of the indicator in the list.
+    indicator_colors : list of strs or None, optional
+        List of colors for the boxes representing the indicators.
+        If None, the colors will
+        be white for all indicators.
+
+    Note
+    ----
+    You can use plt.figure before calling this function to create
+        a new figure with properties you want.
+    You should run plt.show() to display the plot after calling
+        this function.
+
+    Example
+    -------
+    ```
+    plt.figure(figsize=(15, 20))
+    plot_boxes([indicators_1, indicators_2],
+                indicator_names=['facies1', 'facies2'],
+                indicator_colors=['lightblue', 'lightgreen'])
+    plt.show()
+    ```
+    Will plot the boxes for the two indicators dicts. Each different
+    key in the indicators will correspond to a different subplot.
+    The boxes for indicator_1 will be named 'facies1' and colored
+    light blue, and the boxes for indicator_2 will be named 'facies2'
+    and colored light green.
+    """
+    # Get all indicators names (preserving order)
+    ind_names = [ind_name for indicators in indicators_list
+                 for ind_name in indicators.keys()]
+    ind_names = list(OrderedDict.fromkeys(ind_names))
+
+    for i, ind_name in enumerate(ind_names):
+        # values: list of values for the indicator ind_name
+        #     for each indicators if exists
+        # labels: names of the indicators that contains
+        #     the indicator ind_name
+        # colors: colors of the indicators that contains
+        #    the indicator ind_name
+        values, labels, colors = [], [], []
+        for ind_dict, indicators in enumerate(indicators_list):
+            if ind_name in indicators:
+                # The indicator is in indicators : add the values
+                values.append(indicators[ind_name])
+                if indicator_names is not None:
+                    labels.append(indicator_names[ind_dict])
+                else:
+                    labels.append(str(ind_dict + 1))  # + 1 to start at 1
+                if indicator_colors is not None:
+                    colors.append(indicator_colors[ind_dict])
+
+        axi = plt.subplot(len(ind_names) // 4 + 1, 4, i + 1)
+        # patch_artist = False iif colors = [] iif indicator_colors = None
+        patch_artist = colors
+        bplot = axi.boxplot(values, labels=labels, whis=[10, 90],
+                            showfliers=False, patch_artist=patch_artist)
+        if colors:
+            for patch, color in zip(bplot['boxes'], colors):
+                patch.set_facecolor(color)
+        plt.title(ind_name)
