d3a5c6bcf8fc885c9dc3729ea832b92bdd95c58e
Valentin Goldité <valentin.goldite@gmail.com>
2022-09-27 17:44:58 +0200

    🤡 Mock third party functions to make tests more unitary

--- tests/utils/data/test_data_loader.py
+++ tests/utils/data/test_data_loader.py
@@ -1,13 +1,15 @@
 """Tests for utils/data/data_loader.py."""
 
 import os
+from typing import Tuple
 
 import numpy as np
 import pytest
 import pytest_check as check
 import torch
+from pytest_mock import MockerFixture
 
-from utils.configs import Configuration, GlobalConfig
+from utils.configs import GlobalConfig
 from utils.data.data_loader import (DatasetCond2D, DatasetUncond2D,
                                     DistributedDataLoader)
 
@@ -18,13 +20,6 @@ def dataset_path() -> str:
     return 'tests/utils/data/tmp_dataset.npy'
 
 
-@pytest.fixture
-def data_config() -> Configuration:
-    """Return data sub-config object for testing."""
-    return GlobalConfig.build_from_argv(
-        fallback='configs/unittest/data32.yaml').data
-
-
 def create_test_dataset(dataset_path: str) -> None:
     """Create test dataset (if not exist)."""
     if not os.path.exists(dataset_path):
@@ -33,12 +28,31 @@ def create_test_dataset(dataset_path: str) -> None:
         np.save(dataset_path, dataset)
 
 
+def mock_process(mocker: MockerFixture) -> None:
+    """Mock functions from utils.data.process."""
+    data_one_hot = np.random.rand(7, 5, 20, 5).astype(np.float32)
+    data_resize = np.random.rand(10, 20, 5).astype(np.float32)
+    data_crop = np.random.rand(10, 10, 5).astype(np.float32)
+    pixels = np.random.rand(10, 10, 5).astype(np.float32)
+    # normalize output vectors
+    data_resize /= np.sum(data_resize, axis=-1, keepdims=True)
+    data_crop /= np.sum(data_crop, axis=-1, keepdims=True)
+
+    mocker.patch('utils.data.process.to_one_hot_np', return_value=data_one_hot)
+    mocker.patch('utils.data.process.resize_np', return_value=data_resize)
+    mocker.patch('utils.data.process.random_crop_np', return_value=data_crop)
+    mocker.patch('utils.data.process.sample_pixels_2d_np', return_value=pixels)
+
+
 def test_dataset_uncond_2d(dataset_path: str,
-                           data_config: Configuration) -> None:
+                           configs: Tuple[GlobalConfig, GlobalConfig],
+                           mocker: MockerFixture) -> None:
     """Test DatasetUncond2D."""
+    mock_process(mocker)
+    config, _ = configs
     create_test_dataset(dataset_path)
     dataset = DatasetUncond2D(dataset_path=dataset_path, data_size=10,
-                              data_config=data_config,
+                              data_config=config.data,
                               augmentation_fn=lambda x: 2 * x)
     check.equal(len(dataset), 7)
     sample = dataset[0]
@@ -47,7 +61,6 @@ def test_dataset_uncond_2d(dataset_path: str,
     data = sample[0]
     check.is_instance(data, torch.Tensor)
     check.equal(data.size(), (5, 10, 10))
-    check.greater_equal(torch.min(data), 0)
     # Sum of values should be 2 with lambda x: 2 * x augmentation
     print(torch.sum(data, dim=1))
     check.is_true(torch.allclose(torch.sum(data, dim=0),
@@ -56,11 +69,14 @@ def test_dataset_uncond_2d(dataset_path: str,
 
 
 def test_dataset_cond_2d(dataset_path: str,
-                         data_config: Configuration) -> None:
+                         configs: Tuple[GlobalConfig, GlobalConfig],
+                         mocker: MockerFixture) -> None:
     """Test DatasetCond2D."""
+    mock_process(mocker)
+    config, _ = configs
     create_test_dataset(dataset_path)
     dataset = DatasetCond2D(dataset_path=dataset_path, data_size=10,
-                            data_config=data_config,
+                            data_config=config.data,
                             augmentation_fn=lambda x: 2 * x)
     check.equal(len(dataset), 7)
     sample = dataset[0]
@@ -71,7 +87,6 @@ def test_dataset_cond_2d(dataset_path: str,
     check.is_instance(pixel_maps, torch.Tensor)
     check.equal(data.size(), (5, 10, 10))
     check.equal(data.size(), (5, 10, 10))
-    check.greater_equal(torch.min(data), 0)
     # Sum of values should be 2 with lambda x: 2 * x augmentation
     check.is_true(torch.allclose(torch.sum(data, dim=0),
                                  torch.tensor(2, dtype=torch.float32)))
@@ -79,13 +94,16 @@ def test_dataset_cond_2d(dataset_path: str,
 
 
 def test_distributed_dataloader(dataset_path: str,
-                                data_config: Configuration) -> None:
+                                configs: Tuple[GlobalConfig, GlobalConfig],
+                                mocker: MockerFixture) -> None:
     """Test DistributedDataLoader."""
+    mock_process(mocker)
+    config, _ = configs
     create_test_dataset(dataset_path)
     # Case training = True
     dataloader = DistributedDataLoader(
         dataset_path=dataset_path, data_size=10, training=True,
-        data_config=data_config, dataset_class=DatasetUncond2D,
+        data_config=config.data, dataset_class=DatasetUncond2D,
         augmentation_fn=lambda x: 2 * x
     ).loader()
 
@@ -100,7 +118,7 @@ def test_distributed_dataloader(dataset_path: str,
     # Case training = False
     dataloader = DistributedDataLoader(
         dataset_path=dataset_path, data_size=10, training=False,
-        data_config=data_config, dataset_class=DatasetUncond2D,
+        data_config=config.data, dataset_class=DatasetUncond2D,
         augmentation_fn=lambda x: 2 * x
     ).loader()
 
