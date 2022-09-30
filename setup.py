089377bef72fe0a982af466d49cddad1b4c789e4
Valentin Goldité <valentin.goldite@gmail.com>
2022-09-30 18:12:47 +0200

    🆙 Update to release 2.2.0

--- setup.py
+++ setup.py
@@ -1,16 +1,26 @@
 """Setup of gan-face-editing."""
 
-from setuptools import find_packages, setup
+import pathlib
+
+from setuptools import setup
+
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
 
 # Installation
 config = {
-    'name': 'sagan-facies-modeling',
-    'version': '2.1.1',
-    'description': 'Facies modeling with SAGAN.',
+    'name': 'gan-facies',
+    'version': '2.2.0',
+    'description': 'Facies modeling with GAN.',
+    'long_description': README,
+    'long_description_content_type': 'text/markdown',
+    'url': "https://github.com/valentingol/gan-facies-modeling",
     'author': 'Valentin Goldite',
     'author_email': 'valentin.goldite@gmail.com',
-    'packages': find_packages(),
-    'zip_safe': True
+    'packages': ['gan_facies'],
+    'package_dir': {'gan_facies': 'gan_facies'},
 }
 
 setup(**config)
