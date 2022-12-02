289efebaaf4b62eb61db9235207ec1ab33979008
Valentin Goldité <valentin.goldite@gmail.com>
2022-12-02 16:12:20 +0100

    ✅ Update test with pixel_classes

--- tests/utils/test_conditioning.py
+++ tests/utils/test_conditioning.py
@@ -12,9 +12,10 @@ from gan_facies.utils.conditioning import (colorize_pixel_map,
 
 def test_generate_pixel_maps() -> None:
     """Test generate_pixel_maps."""
-    # Case n_pixels is int
+    # Case n_pixels is int, pixel_classes is None
     pixel_maps = generate_pixel_maps(batch_size=2, n_classes=3, n_pixels=5,
-                                     pixel_size=3, data_size=32)
+                                     pixel_size=3, pixel_classes=None,
+                                     data_size=32)
     check.equal(pixel_maps.shape, (2, 3, 32, 32))
     check.equal(pixel_maps.dtype, torch.float32)
     check.is_true((torch.sum(pixel_maps[:, 0], dim=(1, 2)) == 5 * 3 * 3).all())
@@ -31,20 +32,28 @@ def test_generate_pixel_maps() -> None:
     check.is_true(((torch.sum(pixel_maps[:, 1:], dim=1) == 0.0)
                    | (torch.sum(pixel_maps[:, 1:], dim=1) == 1.0)).all())
 
-    # Case n_pixels is list of length 2
+    # Case n_pixels is list of length 2, pixel_classes is not None
     pixel_maps = generate_pixel_maps(batch_size=2, n_classes=3,
-                                     n_pixels=[5, 10], pixel_size=1,
+                                     n_pixels=[5, 10], pixel_classes=[0, 2],
+                                     pixel_size=1,
                                      data_size=32)
     check.is_true((torch.sum(pixel_maps[:, 0], dim=(1, 2)) >= 5.0).all())
     check.is_true((torch.sum(pixel_maps[:, 0], dim=(1, 2)) <= 10.0).all())
+    check.is_true((torch.sum(pixel_maps[:, 1], dim=(1, 2)) == 0.0).all())
 
     # Case invalid n_pixels
     with check.raises(ValueError):
         generate_pixel_maps(batch_size=2, n_classes=3, n_pixels=[5, 10, 15],
-                            pixel_size=1, data_size=32)
+                            pixel_size=1, pixel_classes=None, data_size=32)
     with check.raises(ValueError):
         generate_pixel_maps(batch_size=2, n_classes=3,
                             n_pixels='3', pixel_size=1,  # type: ignore
+                            pixel_classes=None, data_size=32)
+    # Case invalid pixel_classes
+    with check.raises(ValueError):
+        generate_pixel_maps(batch_size=2, n_classes=3,
+                            n_pixels=1, pixel_size=1,
+                            pixel_classes='UNKNOWN',  # type: ignore
                             data_size=32)
 
 
@@ -57,7 +66,8 @@ def test_colorize_pixel_map(mocker: MockerFixture) -> None:
                  return_value=np.random.randint(0, 256, (34 * 5, 34 * 5, 3),
                                                 dtype=np.uint8))
     pixel_maps = generate_pixel_maps(batch_size=25, n_classes=3, n_pixels=5,
-                                     pixel_size=3, data_size=32)
+                                     pixel_size=3, pixel_classes=None,
+                                     data_size=32)
     color_pixel_maps = colorize_pixel_map(pixel_maps)
     check.is_instance(color_pixel_maps, np.ndarray)
     expected_size = 5 * (32 + 2)
